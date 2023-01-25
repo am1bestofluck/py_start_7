1ad9a14da6311ab927fa8ff76645b53324665b73
am_6733 <anton6733@gmail.com>
2023-01-26 00:04:33 +0200

    procrastination why u do it

---
+++ pb_export.py
@@ -0,0 +1,8 @@
+class Entry():
+
+    def __init__(self,):
+        # id,имя,фамилию,номер телефона, комментрий
+        self.name = None
+        self.phone = None
+        self.id_ = None
+        self.comment = None
